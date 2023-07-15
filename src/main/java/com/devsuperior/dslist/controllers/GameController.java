d6542a2c4302d111808e15743e16de4d7579e004
Marcelo Braga <b3darcademy@outlook.com>
2023-07-15 20:34:12 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -4,21 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
-// @RestController => configura essa classe GameController para ser um Controlador.
-// @RequestMapping => configura o caminho que vai ser respondido aqui na minha API.
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
 
 	@Autowired
 	private GameService gameService;
-	
+
+	// Este endpoint vai buscar um objeto por id.
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findGameById(id);
+		return result;
+	}
+
 	// Este endpoint vai buscar os objetos, então será mapeado como GET.
 	@GetMapping
 	public List<GameMinDTO> findAll() {
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -14,16 +16,19 @@ import com.devsuperior.dslist.repositories.GameRepository;
 public class GameService {
 
 	@Autowired
-	private GameRepository gameRepositoy;
-
-	// Método que devolve uma lista de objetos DTO
+	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findGameById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAllGames() {
-		// trazer do banco de dados uma lista de games
-		List<Game> result = gameRepositoy.findAll();
+		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
-		
-		// stream() => é uma lib do Java que permite fazer operações com sequências de dados. A operação .map() transforma objetos de uma coisa para outra.
-		// No caso acima, todo objeto x original, que era Game, eu quero transformar em um new GameMinDTO.
 	}
 }
