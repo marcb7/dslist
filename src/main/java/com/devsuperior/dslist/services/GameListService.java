e0970ae15f1b6a68d1ad3fb8da2b29730dcaefd2
Marcelo Braga <b3darcademy@outlook.com>
2023-07-16 00:22:41 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+
+// @Service => vai registrar a classe GameService como sendo um componente do sistema
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	public List<GameListDTO> findAllLists() {
+		List<GameList> result = gameListRepository.findAll();
+		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+		return dto;
+	}
+}
